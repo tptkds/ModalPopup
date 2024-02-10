e0af27f2462427f127c7a1a6602fd6bbc3b571fa
KimYouGyoung <tptkds12@gmail.com>
2024-02-11 02:01:47 +0900

    모달 팝업 오픈, 클로즈 구현 완료

--- script.js
+++ script.js
@@ -3,4 +3,27 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $modalButton = get('.modal_open_button');
+  const $modal = get('.modal');
+  const $cancelButton = get('.cancel');
+  const $confirmButton = get('.confirm');
+
+  const init = () => {
+    $modalButton.addEventListener('click', openModal);
+    $cancelButton.addEventListener('click', closeModal);
+    $confirmButton.addEventListener('click', closeModal)
+  }
+
+  const openModal = () => {
+    $modal.style.display = 'block';
+    document.body.classList.add('scroll_lock');
+  }
+
+  const closeModal = () => {
+    $modal.style.display = 'none';
+    document.body.classList.remove('scroll_lock');
+  }
+
+  init();
 })()
